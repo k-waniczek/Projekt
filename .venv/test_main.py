af82cc634f1d76da26f898a2c4808b9ec7859dfa
kwaniczek <kamil.wan05@gmail.com>
2026-05-27 11:36:01 +0200

    Dodalem testy

---
+++ .venv/test_main.py
@@ -0,0 +1,26 @@
+import unittest
+from transport import Bike
+from services import BikeService
+
+
+class TestTransportSystem(unittest.TestCase):
+    def test_create_transport(self):
+        """Sprawdza tworzenie transportu i działanie metod."""
+        service = BikeService()
+        transport = service.create_transport()
+
+        self.assertIsInstance(transport, Bike)
+        self.assertEqual(transport.vehicle_type(), "Bike")
+        self.assertEqual(transport.arrival_time(), "5 minutes")
+
+    def test_availability_mechanism(self):
+        """Sprawdza mechanizm dostępności transportu."""
+        service = BikeService()
+
+        self.assertTrue(service.available)
+        service.order_transport()
+        self.assertFalse(service.available)
+
+
+if __name__ == '__main__':
+    unittest.main()
